5b89d9bde4d8635ef01616a7412238d9b7e61fe7
Martín Da Fonte <martindafonte@gmail.com>
2019-02-05 14:48:14 -0300

    test: agrega proyecto de unit test
    Agregar proyecto de test unitario basado en el template de dotnet
    
    close: #2

---
+++ tests/FibonacciExcercise.UnitTests/SampleTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciExcercise.UnitTests
+{
+  [TestClass]
+  public class SampleTest
+  {
+    [TestMethod]
+    public void DummyTestPass()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
