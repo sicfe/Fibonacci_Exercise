2086ff29bb509fbf9504f4954e2bbabf20e91e43
leonela <lepereirap@gmail.com>
2019-02-08 09:50:09 -0300

    feat: se crea la clase e interfaz fibonacci y los respectivos test.
    {issue|close}: #1

---
+++ src/FibonacciWebApi/Services/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FibonacciWebApi.Services
+{
+    public class Fibonacci:IFibonacci
+    {
+        
+        
+        //OJO la implementacion de clases de una interfaz siempre es public
+        public int CalcularFibonacci(int i) {
+            if (i < 0)
+            {
+                throw new Exception(Constantes.obtenerError(Constantes.ERROR_NEGATIVO));
+            }
+            if (i==0 || i==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return CalcularFibonacci(i-1) + CalcularFibonacci(i-2);
+            }
+        }
+
+}
+}
+
+
