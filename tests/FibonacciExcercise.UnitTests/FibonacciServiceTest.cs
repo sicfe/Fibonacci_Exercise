67695d7ab5f774dea9bf21b7bf7f6db7cb096364
Damian <dhernandez@sicfe.uy>
2019-02-11 17:02:06 -0300

    test: se agregan Test Unitarios para casos con valores altos
    issue: #7

--- tests/FibonacciExcercise.UnitTests/FibonacciServiceTest.cs
+++ tests/FibonacciExcercise.UnitTests/FibonacciServiceTest.cs
@@ -9,23 +9,37 @@ namespace FibonacciExcercise.UnitTests
     public void FibonacciTest()
     {
       FibonacciWebApi.Service.FibonacciService f = new FibonacciWebApi.Service.FibonacciService();
-      Assert.AreEqual(5,f.CalcularFibonacci(5));
+      Assert.AreEqual(5,f.CalcularFibonacci(5,0,1));
     }
 
     [TestMethod]
     public void FibonacciTestCero()
     {
       FibonacciWebApi.Service.FibonacciService f = new FibonacciWebApi.Service.FibonacciService();
-      Assert.AreEqual(0,f.CalcularFibonacci(0));
+      Assert.AreEqual(0,f.CalcularFibonacci(0, 0, 1));
     }
 
     [TestMethod]
     public void FibonacciTestNegativo()
     {
       FibonacciWebApi.Service.FibonacciService f = new FibonacciWebApi.Service.FibonacciService();
-      Assert.AreEqual(-1,f.CalcularFibonacci(-5));
+      Assert.AreEqual(-1,f.CalcularFibonacci(-5, 0, 1));
+    }
+
+    [TestMethod]
+    public void FibonacciTestAlto()
+    {
+        FibonacciWebApi.Service.FibonacciService f = new FibonacciWebApi.Service.FibonacciService();
+        Assert.AreEqual(2880067194370816120, f.CalcularFibonacci(90, 0, 1));
+    }
+
+    [TestMethod]
+    public void FibonacciTestMuyAlto()
+    {
+        FibonacciWebApi.Service.FibonacciService f = new FibonacciWebApi.Service.FibonacciService();
+        Assert.AreEqual(-1, f.CalcularFibonacci(91, 0, 1));
     }
 
 
-  }
+    }
 }
