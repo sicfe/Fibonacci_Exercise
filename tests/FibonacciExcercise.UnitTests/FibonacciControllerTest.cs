e0809bacb59f37f4fbf18bfdbaff23a7c0f0bd6b
leonela <lepereirap@gmail.com>
2019-02-11 12:31:14 -0300

    feat: test de controlador fibonacci
    se crea una clase mock y un test para el controlador
    
    issue: #5

---
+++ tests/FibonacciExcercise.UnitTests/FibonacciControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using FibonacciWebApi.Controllers;
+using FibonacciWebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciExcercise.UnitTests
+{
+    [TestClass]
+    public class FibonacciControllerTest
+    {
+        FibonacciMock fib;
+        IFibonacci fib2;
+
+        [TestInitialize]
+        public void Inicializacion()
+        {
+            fib = new FibonacciMock();
+            fib2 = new Fibonacci();
+        }
+               
+
+        [TestMethod]
+        public void GetTest()
+        {
+            int valorEsperado = fib.GetFibonacciMock(); 
+            FibonacciController fibCont = new FibonacciController(fib2);
+            ActionResult<int> valorActual = fibCont.Get(6);
+            Assert.AreEqual(valorEsperado, valorActual.Value); 
+        }
+    }
+}
---
+++ tests/FibonacciExcercise.UnitTests/FibonacciMock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciExcercise.UnitTests
+{
+    class FibonacciMock
+    {
+        public int GetFibonacciMock()
+        {
+            return 13;
+        }
+    }
+}
