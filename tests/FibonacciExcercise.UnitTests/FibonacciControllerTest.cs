67e89f9a646bad002511481ffbfb0babed2c1150
leonela <lepereirap@gmail.com>
2019-02-12 09:08:15 -0300

    test: se modifica el test del controlador
    además se elimina la clase mock

--- tests/FibonacciExcercise.UnitTests/FibonacciControllerTest.cs
+++ tests/FibonacciExcercise.UnitTests/FibonacciControllerTest.cs
@@ -11,23 +11,21 @@ namespace FibonacciExcercise.UnitTests
     [TestClass]
     public class FibonacciControllerTest
     {
-        FibonacciMock fib;
         IFibonacci fib2;
 
         [TestInitialize]
         public void Inicializacion()
         {
-            fib = new FibonacciMock();
             fib2 = new Fibonacci();
         }
                
-
         [TestMethod]
         public void GetTest()
         {
-            int valorEsperado = fib.GetFibonacciMock(); 
+            //int valorEsperado = fib.CalcularFibonacci(7); 
+            int valorEsperado = 13;
             FibonacciController fibCont = new FibonacciController(fib2);
-            ActionResult<int> valorActual = fibCont.Get(6);
+            ActionResult<int> valorActual = fibCont.Get(7);
             Assert.AreEqual(valorEsperado, valorActual.Value); 
         }
     }
