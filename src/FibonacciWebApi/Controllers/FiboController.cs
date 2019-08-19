783b33349872203f22ab989592e19b42861826b5
Gonzalo Guimaraens <gguimaraens@sicfe.uy>
2019-08-19 16:05:18 -0300

    feat: calcula fibonacci
    Devuelve la suma de la secuencia de fibonacci según el número ingresado
    
    {issue|close}: #4

---
+++ src/FibonacciWebApi/Controllers/FiboController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FibonacciWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FiboController : ControllerBase
+    {
+        // GET: api/Fibo
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Ingrese un número en la url" };
+        }
+
+        [HttpGet("{i}")]
+        public int CalcularFibonacci(int i)
+        {
+            int fibo = 1;
+            int fiboPrev = 0;
+            for (int n = 1; n < i; n++)
+            {
+                int fiboPrevPrev = fiboPrev;
+                fiboPrev = fibo;
+                fibo = fiboPrev + fiboPrevPrev;
+            }
+            return fibo;
+        }
+
+
+    }
+}
