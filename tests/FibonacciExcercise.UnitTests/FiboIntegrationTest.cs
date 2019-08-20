634425e4b2d3877f3a2cbcdb07866b5b46fb9f7f
Gonzalo Guimaraens <gguimaraens@sicfe.uy>
2019-08-20 13:05:55 -0300

    test: Creo de test de integración
    Creo test de integración para el servicio
    issue: #8

---
+++ tests/FibonacciExcercise.UnitTests/FiboIntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿using FibonacciWebApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace FibonacciExcercise.UnitTests
+{
+    [TestClass]
+    public class FiboIntegrationTest
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void IntegracionTest()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task LlamadaCorrecta()
+        {
+            var httpResponse = await _client.GetAsync("/api/fibo/10");
+            Assert.IsTrue(httpResponse.IsSuccessStatusCode, "Código erroneo");
+            var responseLong = await httpResponse.Content.ReadAsAsync<long>();
+            Assert.AreEqual(55, responseLong);
+        }
+    }
+}
