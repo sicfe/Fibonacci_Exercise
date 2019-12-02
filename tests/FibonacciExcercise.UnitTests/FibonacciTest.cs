92a90f7cb1a7d9534cd9ae10a09637601e945f64
Romina Fernandez <rfernandez@sicfe.uy>
2019-12-02 16:50:19 -0300

    feat: Se agrega FibonacciTest.
    Se crean 7 test unitarios en FibonacciTest.
    
    Issue: #7

---
+++ tests/FibonacciExcercise.UnitTests/FibonacciTest.cs
@@ -0,0 +1,27 @@
+﻿using FibonacciWebApi.Servicios;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciExcercise.UnitTests
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        [TestMethod]
+        public void FibonacciTestPass()
+        {
+            IFibonacciServicios f = new FibonacciServicios();
+            Assert.AreEqual(f.CalcularFibonacci(0), 0);
+            Assert.AreEqual(f.CalcularFibonacci(1), 1);
+            Assert.AreEqual(f.CalcularFibonacci(4), 3);
+            Assert.AreEqual(f.CalcularFibonacci(8), 21);
+            Assert.AreEqual(f.CalcularFibonacci(30), 832040);
+            Assert.AreEqual(f.CalcularFibonacci(-4), -1);
+            Assert.AreEqual(f.CalcularFibonacci(-10), -1);
+        }
+
+
+    }
+}
