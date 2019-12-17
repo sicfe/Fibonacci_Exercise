35c2c073d10c3691551bf27adde28c474d262ccc
anthony_pastor <apastor@sicfe.uy>
2019-12-17 14:42:10 -0300

    fix: Creación de Controlador Fibonacci
    He creado el Controlador Fibonacci que utiliza la interfaz Servicios, llamando a la función CalcularFibonacci. Luego de este cambio, se devuelve el cálculo de fibonacci como respuesta a la URL: /api/fibonacci/{número}
    
    issue: #2

---
+++ src/FibonacciWebApi/Controllers/FibonacciController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FibonacciWebApi.Services;
+
+namespace FibonacciWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FibonacciController : ControllerBase
+    {
+        // GET api/Fibonacci/5
+        [HttpGet("{número}")]
+        public ActionResult<string> Get(int número)
+        {
+            Servicios ser = new Fibonacci();
+            int res = ser.CalcularFibonacci(número);
+            return res.ToString();
+        }
+    }
+}
