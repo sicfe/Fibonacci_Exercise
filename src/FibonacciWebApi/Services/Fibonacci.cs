5b7ab73c4586ecc12d090a76c26f12745a1f8e41
anthony_pastor <apastor@sicfe.uy>
2019-12-17 14:18:50 -0300

    fix: Implementa un Service que resuelve Fibonacci
    He creado una Interfaz llamada Servicios donde estarán todas las funciones a implementar. También he creado una clase llamada Fibonacci donde implemento la función CalcularFibonacci, la cual calcula fibonacci hasta el índice que es pasado por parámetro.
    
    issue: #1

---
+++ src/FibonacciWebApi/Services/Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+namespace FibonacciWebApi.Services
+{
+    public class Fibonacci : Servicios
+    {
+        public int CalcularFibonacci(int i)
+        {
+            if (i == 1)
+                return 1;
+            else if (i == 0)
+                return 0;
+            else
+                return (CalcularFibonacci(i - 2) + CalcularFibonacci(i - 1));
+        }
+
+
+
+
+    }
+}
