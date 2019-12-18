f26a620060e2693b11bc4f01d39763e174179161
anthony_pastor <apastor@sicfe.uy>
2019-12-18 17:47:06 -0300

    test: Creación de los test de Integración
    Se ha creado una clase en FibonacciExcercise.IntegrationTests llamada ControllersTest, en la cual se crea un servidor con su cliente, el cual hace una petición y verificamos si la respuesta es correcta (es un 200) y si el valor devuelto es el valor correcto de la función fibonacci.
    
    issue: #5

---
+++ FibonacciExcercise.IntegrationTests/ControllersTest.cs
@@ -0,0 +1,35 @@
+using FibonacciWebApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FibonacciExcercise.IntegrationTests
+{
+    [TestClass]
+    public class ControllersTest
+    {
+        private TestServer _testServer;
+
+        public ControllersTest()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Test")
+                .UseStartup<Startup>();
+            _testServer = new TestServer(builder);
+        }
+
+        [TestMethod]
+        public async Task FibonacciGetExito()
+        {
+            HttpClient httpClient = _testServer.CreateClient();
+            var respuesta = await httpClient.GetAsync("api/fibonacci/5");
+            Assert.AreEqual(HttpStatusCode.OK, respuesta.StatusCode);
+            var valorRespuesta = await respuesta.Content.ReadAsStringAsync();
+            Assert.AreEqual(5, Convert.ToInt32(valorRespuesta));
+        }
+    }
+}
