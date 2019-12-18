99d52b16c7119a552731dd411a3cbd1a381045a7
anthony_pastor <apastor@sicfe.uy>
2019-12-18 15:36:50 -0300

    refactor: Controlar que el número ingresado es positivo
    Se le agrega un try-catch a la función CalcularFibonacci en la clase Fibonacci de Services. Si el número ingresado es menor a 0, se lanza un
    ArgumentOutOfRangeException.

--- src/FibonacciWebApi/Services/Fibonacci.cs
+++ src/FibonacciWebApi/Services/Fibonacci.cs
@@ -8,16 +8,23 @@ namespace FibonacciWebApi.Services
     {
         public int CalcularFibonacci(int i)
         {
-            if (i == 1)
-                return 1;
-            else if (i == 0)
-                return 0;
-            else
-                return (CalcularFibonacci(i - 2) + CalcularFibonacci(i - 1));
+            try
+            {
+                if (i < 0)
+                    throw new ArgumentOutOfRangeException();
+                else if (i == 1)
+                    return 1;
+                else if (i == 0)
+                    return 0;
+                else
+                    return (CalcularFibonacci(i - 2) + CalcularFibonacci(i - 1));
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                throw ex;
+            }
+
         }
-
-
-
 
     }
 }
