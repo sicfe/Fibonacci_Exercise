d3e67fec20c70a6701e6d7aac23b35f98d8d7b6c
anthony_pastor <apastor@sicfe.uy>
2019-12-18 16:03:35 -0300

    test: Sea ha creado los test unitarios necesarios
    Sea creado una clase llamada FibonacciUnitTest el cual tiene dos pruebas. El primero prueba los casos bonde, donde se corrobora el resultado al ingresar 1 o 0. El segundo test, prueba si al ingresar un número negativo, se devuelve una excepción del tipo ArgumentOutOfRange.

---
+++ tests/FibonacciExcercise.UnitTests/FibonacciUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FibonacciWebApi.Services;
+
+namespace FibonacciExcercise.UnitTests
+{
+    [TestClass]
+    public class FibonacciUnitTest
+    {
+        [TestMethod]
+        public void FibonacciTestPostivos() 
+        {
+            Servicios ser = new Fibonacci();
+            Assert.AreEqual(0, ser.CalcularFibonacci(0));
+            Assert.AreEqual(1, ser.CalcularFibonacci(1));
+            Assert.AreEqual(1, ser.CalcularFibonacci(2));
+        }
+        [TestMethod]
+        public void FibonacciTestNegativos()
+        {
+            ArgumentOutOfRangeException ExceptionEsperada = null;
+            try
+            {
+                Servicios ser = new Fibonacci();
+
+                Assert.AreEqual(0, ser.CalcularFibonacci(-2));
+                Assert.AreEqual(1, ser.CalcularFibonacci(-1));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                ExceptionEsperada = ex;
+            }
+            Assert.IsNotNull(ExceptionEsperada);
+        }
+    }
+
+}
+
+
+
+
