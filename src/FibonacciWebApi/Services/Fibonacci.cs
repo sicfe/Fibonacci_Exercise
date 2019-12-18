9dc679c96c54e68381e86612c75b13dd37a80691
anthony_pastor <apastor@sicfe.uy>
2019-12-18 15:41:47 -0300

    Revert "refactor: Controlar que el número ingresado es positivo"
    This reverts commit 99d52b16c7119a552731dd411a3cbd1a381045a7.

--- src/FibonacciWebApi/Services/Fibonacci.cs
+++ src/FibonacciWebApi/Services/Fibonacci.cs
@@ -8,23 +8,16 @@ namespace FibonacciWebApi.Services
     {
         public int CalcularFibonacci(int i)
         {
-            try
-            {
-                if (i < 0)
-                    throw new ArgumentOutOfRangeException();
-                else if (i == 1)
-                    return 1;
-                else if (i == 0)
-                    return 0;
-                else
-                    return (CalcularFibonacci(i - 2) + CalcularFibonacci(i - 1));
-            }
-            catch(ArgumentOutOfRangeException ex)
-            {
-                throw ex;
-            }
-
+            if (i == 1)
+                return 1;
+            else if (i == 0)
+                return 0;
+            else
+                return (CalcularFibonacci(i - 2) + CalcularFibonacci(i - 1));
         }
+
+
+
 
     }
 }
