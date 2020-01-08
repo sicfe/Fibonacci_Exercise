afc9d21a44a75a0363546a1d188625be9c7a3135
facabral <facabral17@gmail.com>
2020-01-08 10:15:16 -0300

    feat: Agrega services para resolver Fibonacci
    issue: #1

---
+++ src/FibonacciWebApi/Services/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FibonacciWebApi.Services
+{
+    public class Fibonacci : IFibonacciServices
+    {
+        public int SecuenciaFibo(int numero)
+        {
+            if (numero < 0)
+            {
+                return -1;
+            }
+            if (numero > 90)
+            {
+                return -1;
+            }
+            if (numero == 0)
+            {
+                return 0;
+            }
+            if (numero == 1)
+            {
+                return 1;
+            }
+
+            return SecuenciaFibo (numero -1) + SecuenciaFibo (numero -2);
+        }
+    }
+}
