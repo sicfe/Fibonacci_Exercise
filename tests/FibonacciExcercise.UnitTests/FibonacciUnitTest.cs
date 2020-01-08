89e3c26060aaabd702437d6a1b4adbf04eafea21
Facundo Cabral <fcabral@sicfe.uy>
2020-01-08 10:42:53 -0300

    feat: Agrega Unit Tests
    issue: #4

---
+++ tests/FibonacciExcercise.UnitTests/FibonacciUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using FibonacciWebApi.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciExcercise.UnitTests
+{
+    [TestClass]
+    public class FibonacciUnitTest
+    {
+        [TestMethod]
+        public void FibonacciPositivo()
+        {
+            IFibonacciServices Fibo = new Fibonacci();
+            Assert.AreEqual(0, Fibo.SecuenciaFibo(0));
+            Assert.AreEqual(55, Fibo.SecuenciaFibo(10));
+        }
+
+        [TestMethod]
+        public void FibonacciNegativo()
+        {
+            IFibonacciServices Fibo = new Fibonacci();
+            Assert.AreEqual(-1, Fibo.SecuenciaFibo(-1));
+            Assert.AreEqual(-1, Fibo.SecuenciaFibo(-5));
+            Assert.AreEqual(-1, Fibo.SecuenciaFibo(-1));
+        }
+    }
+}
