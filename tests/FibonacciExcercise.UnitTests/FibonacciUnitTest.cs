cfb1385e165610b06939e70782e7cdf3338d6fdb
Facundo Cabral <fcabral@sicfe.uy>
2020-01-09 10:06:13 -0300

    test: Mejora en medición de tiempo y mensajes de error
    Implementé Stopwatch para tener un mejor control sobre el tiempo de cálculo para Fibonacci 30.
    Ahora también se puede comprobar que los mensajes de error correspondan al su error.

--- tests/FibonacciExcercise.UnitTests/FibonacciUnitTest.cs
+++ tests/FibonacciExcercise.UnitTests/FibonacciUnitTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace FibonacciExcercise.UnitTests
@@ -28,6 +29,7 @@ namespace FibonacciExcercise.UnitTests
             Assert.AreEqual(false, Fibo.SecuenciaFibo(-5).Exito);
             Assert.AreEqual(Constantes.ErrorMenor0, Fibo.SecuenciaFibo(-5).Mensage);
             Assert.AreEqual(false, Fibo.SecuenciaFibo(-1).Exito);
+            Assert.AreEqual("El valor no puede ser negativo", Constantes.ErrorMenor0);
         }
 
         [TestMethod]
@@ -35,15 +37,21 @@ namespace FibonacciExcercise.UnitTests
         {
             IFibonacciServices Fibo = new Fibonacci();
             Assert.AreEqual(false, Fibo.SecuenciaFibo(91).Exito);
+            Assert.AreEqual("El valor debe ser menor a 90", Constantes.ErrorMayor90);
         }
 
         [TestMethod]
         [DataRow(832040, 30, DisplayName = "Fibonacci 30 = 832040")]
         public void FibonacciTreinta(int esp, int paso)
         {
+            Stopwatch tiempo = new Stopwatch();
+            tiempo.Start();
             IFibonacciServices Fibo = new Fibonacci();
             Assert.AreEqual(esp, Fibo.SecuenciaFibo(paso).Resultado);
             Assert.AreEqual(true, Fibo.SecuenciaFibo(paso).Exito);
+            tiempo.Stop();
+            TimeSpan ts = tiempo.Elapsed;
+            Console.WriteLine("Tiempo: " + tiempo.Elapsed + " segundos");
         }
     }
 }
