be45272e5a6aa23a868d9ce1687cc52353c549d8
Rodrigo Gabin <rgabin@sicfe.uy>
2020-07-02 11:28:23 -0300

    feat: Creo la clase Fibonacci Controller
    Este commit crea esta clase para implementar el servicio que resuelve el método CalcularFibonacci
    
    {issue} #2

---
+++ src/FibonacciWebApi/Controllers/FibonacciController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FibonacciWebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FibonacciWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FibonacciController : ControllerBase
+    {
+        [HttpGet("{fibo}")]
+        public ActionResult<long> Get(long fibo)
+        {
+            IFibonacci fibonacci = new Fibonacci();
+            return fibonacci.CalcularFibonacci(fibo);
+        }
+    }
+}
