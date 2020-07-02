7bc782931a2b39c489db9bcddb4c671c060f507b
Rodrigo Gabin <rgabin@sicfe.uy>
2020-07-02 11:05:31 -0300

    feat: Creo una nueva clase Fibonacci
    Este commit crea una nueva clase Fibonacci con el método CalcularFibonacci(int) que dado un entero retorna el valor para dicho entero en la serie de Fibonacci
    
    {issue|close}: #1

---
+++ src/FibonacciWebApi/Services/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FibonacciWebApi.Services
+{
+    public class Fibonacci : IFibonacci
+    {
+        
+        public
+            
+        long CalcularFibonacci(long fibo)
+        {
+            if (fibo == 0)
+                return 0;
+            else if (fibo == 1)
+                return 1;
+            else
+                return (CalcularFibonacci(fibo - 2) + CalcularFibonacci(fibo - 1));
+            
+        }
+
+    }
+}
