0abf66b8b80df1af7fba31c9d4c5375dc13cfeef
Rodrigo Gabin <rgabin@sicfe.uy>
2020-07-06 16:14:09 -0300

    feat: Mejoras en el test unitario
    Agrego los casos para números negativos y mayores a noventa
    
    { issue: close } : #4

--- tests/FibonacciExcercise.UnitTests/FibonacciTest.cs
+++ tests/FibonacciExcercise.UnitTests/FibonacciTest.cs
@@ -19,6 +19,8 @@ namespace FibonacciExcercise.UnitTests
             Assert.IsTrue((2880067194370816120 == fibonacci.CalcularFibonacci(90)));
             Assert.IsTrue((102334155 == fibonacci.CalcularFibonacci(40)));
             Assert.IsTrue((12586269025 == fibonacci.CalcularFibonacci(50)));
+            Assert.IsTrue(-1 == fibonacci.CalcularFibonacci(91));
+            Assert.IsTrue(-1 == fibonacci.CalcularFibonacci(-1));
 
         }
 
