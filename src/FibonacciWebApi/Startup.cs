feebed2878b77b02b2663c06941c2a63d88dedeb
Rodrigo Gabin <rgabin@sicfe.uy>
2020-07-06 12:49:48 -0300

    feat: Agrego servicio fibonacci a startup
    A través del método ConfigureServices de la clase startup agrego el servicio de CalcularFibonacci a la api
    
    { issue: close } : #6

--- src/FibonacciWebApi/Startup.cs
+++ src/FibonacciWebApi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FibonacciWebApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,7 @@ namespace FibonacciWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IFibonacci, Fibonacci>();//Agrego el servicio fibonacci a la api(vista)
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
