232b0f964e10ef258685d7911859e75374d56f1b
Rodrigo Gabin <rgabin@sicfe.uy>
2020-07-06 15:32:09 -0300

    feat: Optimizo la clase fibonacci
    Los cálculos ahora son inmediatos y se controlan los casos de borde.
    
    { issue : close } : #7

--- src/FibonacciWebApi/Services/Fibonacci.cs
+++ src/FibonacciWebApi/Services/Fibonacci.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,18 +8,30 @@ namespace FibonacciWebApi.Services
 {
     public class Fibonacci : IFibonacci
     {
-        
+
         public
-            
-        long CalcularFibonacci(long fibo)
+
+        long  CalcularFibonacci(int fibo)
         {
-            if (fibo == 0)
-                return 0;
-            else if (fibo == 1)
-                return 1;
-            else
-                return (CalcularFibonacci(fibo - 2) + CalcularFibonacci(fibo - 1));
-            
+            try
+            {
+                if ((fibo < 0) || (fibo > 90))
+                    throw new ArgumentOutOfRangeException("El valor ingresado debe ser mayor o igual a 0 o menor o igual a 90");
+                else if (fibo == 0)
+                    return 0;
+                else if (fibo == 1)
+                    return 1;
+                else if (fibo < 62)
+                    return Convert.ToInt64((1 / Math.Sqrt(5)) * Math.Pow(((1 + Math.Sqrt(5)) / 2), fibo) + (-1 / Math.Sqrt(5)) * Math.Pow(((1 - Math.Sqrt(5)) / 2), fibo));
+                else
+                    return (CalcularFibonacci(fibo - 2) + CalcularFibonacci(fibo - 1));
+
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return -1;// Si el resultado es negativo existe error
+            }
+                
         }
 
     }
